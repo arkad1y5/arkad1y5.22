6529d3602f7550f918cce0ffe1fcdd9b60809784
arkad1y5 <71595331+arkad1y5@users.noreply.github.com>
2022-04-17 15:05:53 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,50 @@
+
+const filter_btns = document.querySelectorAll(".filter-btn");
+const skills_wrap = document.querySelector(".skills");
+const skills_bars = document.querySelectorAll(".skill-progress");
+
+filter_btns.forEach( btn => btn.addEventListener("click", () => {
+    filter_btns.forEach(button => button.classList.remove("active"));
+    btn.classList.add("active");
+
+    let filterValue = btn.dataset.filter;
+    //console.log(filterValue);  перевірка
+    
+    $(".grid").isotope({filter: filterValue});
+  })
+);
+
+$(".grid").isotope({
+  itemSelector: ".grid-item",
+  layoutMode: "fitRows",
+  transitionDuration: "0.6s",
+});
+
+window.addEventListener("scroll", () => {
+  skillsEffect();
+  countUp();
+});
+
+function checkScroll(el) {
+  let rect = el.getBoundingClientRect();
+  if (window.innerHeight >= rect.top + el.offsetHeight) return true;
+  return false;
+}
+
+function skillsEffect() {
+  if (!checkScroll(skills_wrap)) return;
+  skills_bars.forEach((skill) => (skill.style.width = skill.dataset.progress));
+}
+
+
+
+
+
+
+
+
+$('.grid').isotope({
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows',
+    transitionDuration: "0.6s",
+})
