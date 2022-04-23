405a7fa3e7b15f243643b9dfdb3efede365f2a53
arkad1y5 <71595331+arkad1y5@users.noreply.github.com>
2022-04-23 20:01:42 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@
 const filter_btns = document.querySelectorAll(".filter-btn");
 const skills_wrap = document.querySelector(".skills");
 const skills_bars = document.querySelectorAll(".skill-progress");
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector("header nav");
+const links = document.querySelectorAll(".links a");
 
 filter_btns.forEach( btn => btn.addEventListener("click", () => {
     filter_btns.forEach(button => button.classList.remove("active"));
@@ -36,6 +39,25 @@ function skillsEffect() {
   skills_bars.forEach((skill) => (skill.style.width = skill.dataset.progress));
 }
 
+//menu hamburder
+function closeMenu() {
+  navbar.classList.remove("open");
+  document.body.classList.remove("stop-scroling");
+}
+
+hamburger_menu.addEventListener("click", () =>{
+  if(!navbar.classList.contains("open")){
+    navbar.classList.add("open");
+    document.body.classList.add("stop-scroling");
+  }
+  else{
+    closeMenu();
+  }
+});
+
+links.forEach((link) => link.addEventListener("click", () => closeMenu()));
+
+//menu hamburger
 
 
 
