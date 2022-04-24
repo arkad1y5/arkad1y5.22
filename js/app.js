f135ad3e232bbeb6df55c4f9e586ee7e5bbece81
arkad1y5 <71595331+arkad1y5@users.noreply.github.com>
2022-04-24 10:59:45 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -5,6 +5,20 @@ const skills_bars = document.querySelectorAll(".skill-progress");
 const hamburger_menu = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector("header nav");
 const links = document.querySelectorAll(".links a");
+const anchors = document.querySelectorAll('a[href*="#"]') //плавний скролынг
+
+//код до плавного скролінгу
+for (let anchor of anchors){
+  anchor.addEventListener("click",function(event){
+    event.preventDefault();
+    const blockID = anchor.getAttribute('href')
+    document.querySelector('' + blockID).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
+};
+
 
 filter_btns.forEach( btn => btn.addEventListener("click", () => {
     filter_btns.forEach(button => button.classList.remove("active"));
@@ -61,12 +75,10 @@ links.forEach((link) => link.addEventListener("click", () => closeMenu()));
 
 
 
-
-
-
-
 $('.grid').isotope({
     itemSelector: '.grid-item',
     layoutMode: 'fitRows',
     transitionDuration: "0.6s",
 })
+
+
