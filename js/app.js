2061ded170dcab78b89fa54e092af268d85b9461
arkad1y5 <71595331+arkad1y5@users.noreply.github.com>
2022-05-09 15:37:35 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -6,6 +6,16 @@ const hamburger_menu = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector("header nav");
 const links = document.querySelectorAll(".links a");
 const anchors = document.querySelectorAll('a[href*="#"]') //плавний скролынг
+const footer_input = document.querySelector(".footer-input");
+
+footer_input.addEventListener("focus", () => {
+  footer_input.classList.add("focus");
+}); 
+
+footer_input.addEventListener("blur", () => {
+  if(footer_input.value != "") return;
+  footer_input.classList.remove("focus");
+}); 
 
 //код до плавного скролінгу
 for (let anchor of anchors){
